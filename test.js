042386046dff8463daa0bf0aadd295f5cf41e97d
chadcarlson <chad.carlson@platform.sh>
2021-02-08 14:43:10 -0500

    Better commit messages.

--- test.js
+++ test.js
@@ -5,11 +5,13 @@ var moment = require('moment');
 // Get document, or throw exception on error
 try {
     let doc = yaml.load(fs.readFileSync('test.yaml', 'utf8'));
-    doc.data.push(moment().format('yyyy-mm-dd:hh:mm:ss'));
+    let newDate = moment().format('yyyy-MM-DD');
+    doc.data.push(newDate);
     let yamlStr = yaml.dump(doc);
     fs.writeFile('test.yaml', yamlStr, (error) => { 
               if (error) throw err; 
     }) 
+    console.log(newDate)
 } catch (e) {
     console.log(e);
 }
