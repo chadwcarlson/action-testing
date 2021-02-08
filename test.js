32e953a70338ac7c68331d5abb8a533b1280a87b
chadcarlson <chad.carlson@platform.sh>
2021-02-08 15:05:38 -0500

    Schedule.

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var moment = require('moment');
 // Get document, or throw exception on error
 try {
     let doc = yaml.load(fs.readFileSync('test.yaml', 'utf8'));
-    let newDate = moment().format('yyyy-MM-DD');
+    let newDate = moment().format('yyyy-MM-DD - mm:ss');
     doc.data.push(newDate);
     let yamlStr = yaml.dump(doc);
     fs.writeFile('test.yaml', yamlStr, (error) => { 
