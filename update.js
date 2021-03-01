014ad968eecd544a229aa935f83bb36ff157b45a
chadcarlson <chad.carlson@platform.sh>
2021-03-01 14:29:22 -0500

    Conditional update.

--- update.js
+++ update.js
@@ -4,16 +4,19 @@ const yaml = require('js-yaml');
 
 const dataFile = "data.yaml";
 
-try {
-    // Load and update data.
-    let doc = yaml.load(fs.readFileSync(dataFile, 'utf8'));
-    let newDate = moment().format('yyyy-MM-DD - mm:ss');
-    doc.data.push(newDate);
-    // Re-write the data file.
-    let yamlStr = yaml.dump(doc);
-    fs.writeFile(dataFile, yamlStr, (error) => { 
-        if (error) throw err; 
-    }) 
-} catch (e) {
-    console.log(e);
+// Conditional (to mimic no-update days). Only update on odd days.
+if ((Number(moment().format('DD')) % 2) == 1) {
+    try {
+        // Load and update data.
+        let doc = yaml.load(fs.readFileSync(dataFile, 'utf8'));
+        let newDate = moment().format('yyyy-MM-DD - mm:ss');
+        doc.data.push(newDate);
+        // Re-write the data file.
+        let yamlStr = yaml.dump(doc);
+        fs.writeFile(dataFile, yamlStr, (error) => { 
+            if (error) throw err; 
+        }) 
+    } catch (e) {
+        console.log(e);
+    }    
 }
