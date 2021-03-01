fbf5e9580d1dd6028eaf15a50d572b9f02dc45ac
chadcarlson <chad.carlson@platform.sh>
2021-03-01 14:44:34 -0500

    test.

--- update.js
+++ update.js
@@ -5,7 +5,7 @@ const yaml = require('js-yaml');
 const dataFile = "data.yaml";
 
 // Conditional (to mimic no-update days). Only update on odd days.
-if ((Number(moment().format('DD')) % 2) == 0) {
+if ((Number(moment().format('DD')) % 2) == 1) {
     try {
         // Load and update data.
         let doc = yaml.load(fs.readFileSync(dataFile, 'utf8'));
